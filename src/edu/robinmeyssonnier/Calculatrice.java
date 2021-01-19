7533b6df3c3eae77ce15626992269bc3b15a26a9
Robin Meyssonnier <robin.meyssonnier@outlook.com>
2021-01-19 12:05:17 +0100

    [ADD] Add build.xml

--- src/edu/robinmeyssonnier/Calculatrice.java
+++ src/edu/robinmeyssonnier/Calculatrice.java
@@ -2,6 +2,12 @@ package edu.robinmeyssonnier;
 
 public class Calculatrice {
 
+    /**
+     *
+     * @param a one integer
+     * @param b second integer
+     * @return addition result
+     */
     public int addition (int a, int b) {
         return a + b;
     }
